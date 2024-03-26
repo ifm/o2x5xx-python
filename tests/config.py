498bfb4de71f9853443546f454ad08fb0f7d54f3
Gann <michael.gann@ifm.com>
2024-03-26 09:27:00 +0100

    Set config flag importDeviceConfigUnittests to default value True

--- tests/config.py
+++ tests/config.py
@@ -2,5 +2,5 @@ deviceAddress = '192.168.1.72'
 pcicTcpPort = 50010
 maxNumberContainers = 9
 # Set this to False if the applications for the unittests are already imported on the sensor
-importDeviceConfigUnittests = False
+importDeviceConfigUnittests = True
 
