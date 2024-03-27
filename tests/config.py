c40292017467ab28b24a9a854b758c6249d2aab3
Galoshi <michael.gann@fm.com>
2024-03-27 09:35:10 +0100

    Setting default ip in config file for unittests

--- tests/config.py
+++ tests/config.py
@@ -1,4 +1,4 @@
-deviceAddress = '192.168.1.72'
+deviceAddress = '192.168.0.69'
 pcicTcpPort = 50010
 maxNumberContainers = 9
 # Set this to False if the applications for the unittests are already imported on the sensor
